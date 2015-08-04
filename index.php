fdb4dc650f69ecbf8e14555d7497843b932ba4f7
dadmor <dadmor@gmail.com>
2015-08-04 11:58:43 +0200

    standalone edition

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
-<script type="text/javascript" src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!-- <script type="text/javascript" src="//code.jquery.com/jquery-1.11.1.min.js"></script> -->
+<script type="text/javascript" src="js/jquery.1.11.1.min.js"></script>
 <script type="text/javascript" src="js/alpaca-core.min.js"></script>
 <script type="text/javascript" src="js/lodash.js"></script>
 <script type="text/javascript" src="js/lodash-deep.js"></script>
-<link type="text/css" href="//code.cloudcms.com/alpaca/1.5.11/bootstrap/alpaca.min.css" rel="stylesheet"/>
+<link type="text/css" href="js/alpaca-min.css" rel="stylesheet"/>
 
+<body>
 
+<h1>Form creator</h1>
 
 
 <div id="main_container" style="float:left; width:70%">
@@ -17,10 +20,17 @@
 	<a id="add_select" href="#" class="button action" style="width:200px; margin-bottom:2px">multi choice</a><Br/>
 	<a id="add_checkbox" href="#" class="button action" style="width:200px; margin-bottom:2px">single checkbox</a><Br/>
 	<a id="add_object" href="#" class="button action" style="width:200px; margin-bottom:2px">container ( fieldset )</a><Br/>
-	<textarea id="output"></textarea>
 </div>
+<br style="clear:both"/>
+<h2>Form schema and options output</h2>
+<table style="width:100%">
+	<tr>
+		<td>Schema<textarea id="schema_output"></textarea></td>
+		<td>Options<textarea id="options_output"></textarea></td>
+	</tr>
+</table>	
 
-	
+</body>
 
 <style>
 /* normalize - delete this section if you wand add editor into WordPress plugins*/
@@ -52,14 +62,26 @@ input[type="text"]:focus {
   outline: none;
 }
 
-#output{
+#schema_output, #options_output{
 	
 	width:100%;
 	height:300px;
-	display:none;
+	border:1px solid #ccc;
+	font-size:11px;
+
 }
 /* ------------------------------------------------------------ */
+/* extra standalone */
+.helper-item-details select{
+	border: 1px solid #dcdcdc;
+	background-color: #fff;
+	padding: 3px 1px;
+	font-size: 14px;
+	width: 100%;
+}
+body{font-family:arial;}
 
+/* ------------------------------------------------------------ */
 .alpaca-fieldset-item-container{
 	position:relative;
 	border:1px solid #B3B3B3; 
@@ -124,29 +146,34 @@ fieldset{
 	margin-left:-5px;
 	position:relative;
 }
+.helper-item-details .context-mnu{
+	width:19%; height:100%; position:absolute; left:0; top:0; border-right:1px solid #ccc; background-color:#fff
+}
+.helper-item-details .context-mnu-item{
+	padding:5px; border-bottom:1px solid #ccc
+}
+
+.helper-item-details .label{
+	width:120px; line-height:30px; float:left
+}
 
 </style>
 
 <script type="text/javascript">
 			jQuery(document).ready(function($) {
-alert('jestem')
+				
 				window.fieldsCounter = 0;
 				window.targetPath = ['schema.properties'];
 				var data = {
 			    /* ----------------------------------------------------------------------- */
 			    	"options": {
-			    		"ajax_callback": {
-		               		"dependence_three": {
-		                    	"rightLabel": "Messages"
-		               		},
-		               	}
+						"fields": {}
 			    	},
 			    	"schema": {
 				      //"title": "Form extended options",
 				      //"description": "Define your special display properties",
 				      "type": "object",
-				      "properties": {
-	        
+				      "properties": {	        
 				        "ajax_callback": {
 							"type": "string",
 							"title": "callback AJAX function name",
@@ -164,115 +191,55 @@ alert('jestem')
 				    }
 				}
 
-				funcrion_render_alpaca = function (data){
-
-					data["postRender"] = function(control){
-						swith_fields_to_min_mode(control);
-					}
-					$("#output").text(JSON.stringify(data));
-
-			    	$("#main_container").alpaca(data);
-		        }
-
-		        funcrion_render_alpaca(data);
+				
 	            /* ----------------------------------------------------------------------- */
 
-	            $('#add_input').click(function(){
-				    add_new_element('string','');
-				});
+	           
 
-				$('#add_select').click(function(){
-					add_new_element('string',['option1','option2','option3']);
-				});
-
-				$('#add_checkbox').click(function(){
-					add_new_element('boolean','');
-				});
-
-				$('#add_object').click(function(){
-					add_new_element('object','');
-				});
-
-				$(document).on("click", "div.helper-fieldset-tab", function(e) { 
-				//$('fieldset').live('click', function(e) {
-					
-					//if(e.srcElement.localName == 'fieldset'){ 
-
-
-						
-						window.targetPath = [];
-						if($(this).attr('data-first-container') != 'true'){
-							objects_array(e.target);
-						}else{
-							window.targetPath = 'schema.properties';
-						}
-						e.stopPropagation();
-
-						alert(window.targetPath);
-
-					//}
-
-				});
-				$(document).on("click", "div.helper-object-remove", function(e) { 
-				//$('.helper-object-remove').live('click', function(e) {
-
-
-					
-					if($(this).closest('fieldset').attr('data-first-container') != 'true'){
-						window.targetPath = '';
-						objects_array($(this).closest('fieldset'));
-					}else{
-						window.targetPath = 'schema.properties';
-					}
-					deepDelete(window.targetPath+'.'+$(this).closest('.alpaca-fieldset-item-container').attr('data-alpaca-item-container-item-key'), data);
-					$('#main_container').children().remove();
-				    funcrion_render_alpaca(data);
-
-				});
-
-				$(document).on("click", "div.alpaca-fieldset-item-container", function(e) { 
-				//$(".alpaca-fieldset-item-container").live('click', function(e) {                 
-
+			window._parent;				
+			var _UXFORM = {
+			    open_field_options : function(_this){
+			    	//console.log('%c -- open_field_options ------', 'background: #222; color: #bada55');               
 			        //tb_show('Field options','#TB_inline?height=360&width=300&inlineId=prev');
 			        var input ='<div class="helper-item-details">';
-				        input += '<div style="width:19%; height:100%; position:absolute; left:0; top:0; border-right:1px solid #ccc; background-color:#fff"> ';
-				        	input += '<div style="padding:5px; border-bottom:1px solid #ccc">Basic</div>';
-				        	input += '<div style="padding:5px; border-bottom:1px solid #ccc">Advanced</div>';
-				        	input += '<div style="padding:5px; border-bottom:1px solid #ccc">Dependency</div>';
-				        	input += '<div style="padding:5px; border-bottom:1px solid #ccc">Specials</div>';
+				        input += '<div class="context-mnu"> ';
+				        	input += '<div class="context-mnu-item">Basic</div>';
+				        	input += '<div class="context-mnu-item">Advanced</div>';
+				        	input += '<div class="context-mnu-item">Dependency</div>';
+				        	input += '<div class="context-mnu-item">Specials</div>';
 				        input += '</div>';
 				        input += '<div style="width:80%; float:right">';
-				       	 	input += '<label style="width:120px; line-height:30px; float:left">name</label><input type="text"><br/>';
-				        	input += '<label style="width:120px; line-height:30px; float:left"">label</label><input type="text"><br/>';
-				        	input += '<label style="width:120px; line-height:30px; float:left"">field subtype</label><input type="text"><br/>';
-				        	input += '<label style="width:120px; line-height:30px; float:left"">placeholder</label><input type="text"><br/>';
-				        	input += '<label style="width:120px; line-height:30px; float:left"">default value</label><input type="text"><br/>';
+				        	
+				        	input += '<label class="label">type</label>';
+				        	input += '<select><option>Array</option><option>Checkbox</option><option>File</option><option>Hidden</option></select><br/>';
+				       	 	
+				       	 	input += '<label class="label">name</label>';
+				       	 	input += '<input class="input-helper" type="text" name="name" data-type="shema-key">';
+				        	
+				        	input += '<label class="label">label</label>';
+				        	input += '<input class="input-helper" type="text" name="label" data-type="option">';
+				        	
+				        	input += '<label class="label">placeholder</label>';
+				        	input += '<input class="input-helper" type="text" name="placeholder" data-type="option">';
+				        	
+				        	input += '<label class="label">default value</label><input type="text">';
 						input += '</div>';
 						input += '<br style="clear:both">';
 					input += '</div>';
 					$('.alpaca-fieldset-item-container .helper-item-details').remove();
-			        $(this).append(input);
+			        _this.append(input);
 
-			    });
+			    },
 
-				$(document).on("click", "div.helper-item-details", function(e) { 
-
-				//$(".helper-item-details").live('click', function(e) {                 
-			        e.stopPropagation();
-			    });
-
-
-
-
-				function add_new_element( type, _enum ){
-					
+				add_new_element : function( type, _enum ){
+					//console.log('%c -- add_new_element ----------', 'background: #222; color: #bada55');
 					//path = window.targetPath + '.' + "new_" + type + "_" + window.fieldsCounter;
 					path = window.targetPath + '.' + "element_" + window.fieldsCounter;
+					//console.log('%c path:'+path, 'background: #ccc; color: blue');
 
-					 _.deepSet(data, path+'.type', type);
-					 _.deepSet(data, path+'.title', "New element");
-					 _.deepSet(data, path+'.description', "example description");
-
+					_.deepSet(data, path+'.type', type);
+					_.deepSet(data, path+'.title', "New element");
+					_.deepSet(data, path+'.description', "example description");
 
 				    if(_enum != ''){
 				    	_.deepSet(data, path+'.enum', _enum);
@@ -287,47 +254,68 @@ alert('jestem')
 				    window.fieldsCounter ++;
 				    
 				    $('#main_container').children().remove();
+
+				    //console.log(data);
 				    funcrion_render_alpaca(data);
-				}
+				},
+				change_path_deep : function(_this){
 
-				var parent;
-				
-				function objects_array( _this ){
-						
-						target = $(_this).attr('data-path');
-						target +=  window.targetPath;
+					window.targetPath = [];
+					if( _this.parent('fieldset').attr('data-first-container') != 'true'){
+						this.objects_array(_this);
+					}else{
+						window.targetPath = 'schema.properties';
+					}
+				},
 
-						window.targetPath = '.' + target;
+				remove_element : function( _this ){
+					
+					if(_this.closest('fieldset').attr('data-first-container') != 'true'){
+						//window.targetPath = '';
+						//objects_array(_this.closest('fieldset'));
+					}else{
+						window.targetPath = 'schema.properties';
+					}
+					this.deepDelete(window.targetPath+'.'+_this.closest('.alpaca-fieldset-item-container').attr('data-alpaca-item-container-item-key'), data);
+					$('#main_container').children().remove();
+				    funcrion_render_alpaca(data);
 
-						parent = $(_this).parents('fieldset');
+				},
 
-						if($(parent).attr('data-path') != 'schema.properties'){
-							
-							objects_array(parent);
-						
-						}else{
-							
-							window.targetPath = 'schema.properties'+window.targetPath
-							return false;
-						}					
+				objects_array : function( _this ){
+					
+					target = _this.attr('data-path');
+					target +=  window.targetPath;
+					window.targetPath = '.' + target;
+					window._parent = _this.parent('fieldset').parents('fieldset').children('.helper-fieldset-tab');
 
-				}
+					if(window._parent.attr('data-path') != 'schema.properties'){
+						this.objects_array(window._parent);
+					}else{
+						window.targetPath = 'schema.properties'+window.targetPath
+						return false;
+					}
+				},
 
-				function deepDelete(target, context) {
+				deepDelete : function(target, context) {
 				  context = context || window;
 				  var targets = target.split('.');
 				  if (targets.length > 1)
-				    deepDelete(targets.slice(1).join('.'), context[targets[0]]);
+				    this.deepDelete(targets.slice(1).join('.'), context[targets[0]]);
 				  else
 				    delete context[target];
-				}
+				},
 
-				function remove_element(path_to_element){
+				deepADD : function(target, context) {
+				  context = context || window;
+				  var targets = target.split('.');
+				  if (targets.length > 1)
+				    this.deepDelete(targets.slice(1).join('.'), context[targets[0]]);
+				  else
+				    delete context[target];
+				},
 
-				}
-
-
-				function swith_fields_to_min_mode(){
+				swith_fields_to_min_mode : function (){
 
 					/* build first fieldset */
 					$("#main_container").children('fieldset').attr('data-first-container','true');
@@ -364,13 +352,76 @@ alert('jestem')
 						}
 					});
 				
+				},
+
+				add_option : function(){
+					alert(window.targetPath)
 				}
 
-				function swith_fields_to_normal_mode(){
+			};
 
-					$(".alpaca-fieldset-item-container").children().css('display','block');
-				
-				}
-			 
+
+
+			/* ACTIONS EVENTS HANDLERS */
+
+			$(document).on("click", "div .helper-object-remove", function(e) { 
+				_UXFORM.remove_element($(this));
+				//e.stopPropagation();
 			});
+
+			$(document).on("click", "div.alpaca-fieldset-item-container", function(e) { 
+			//$(".alpaca-fieldset-item-container").live('click', function(e) {  
+				_UXFORM.open_field_options($(this));
+		    });
+
+			$(document).on("click", "div.helper-item-details", function(e) { 
+			//$(".helper-item-details").live('click', function(e) {                 
+		        e.stopPropagation();
+		    });
+
+		    $('#add_input').click(function(){
+			    _UXFORM.add_new_element('string','');
+			});
+
+			$('#add_select').click(function(){
+				_UXFORM.add_new_element('string',['option1','option2','option3']);
+			});
+
+			$('#add_checkbox').click(function(){
+				_UXFORM.add_new_element('boolean','');
+			});
+
+			$('#add_object').click(function(){
+				_UXFORM.add_new_element('object','');
+			});
+
+			$(document).on("click", "div.helper-fieldset-tab", function(e) { 
+			//$('fieldset').live('click', function(e) {
+				_UXFORM.change_path_deep($(this));
+				e.stopPropagation();
+			});
+
+			$(document).on("change", "input.input-helper", function(e) { 
+				console.log(data);
+				console.log(window.targetPath);
+				console.log($(this).val());
+				console.log($(this).attr('name'));
+				_UXFORM.add_option();
+			});
+
+			/* INIT  */
+		    funcrion_render_alpaca = function (data){
+
+				data["postRender"] = function(control){
+					_UXFORM.swith_fields_to_min_mode(control);
+				}
+				$("#schema_output").text(JSON.stringify(data.schema));
+				$("#options_output").text(JSON.stringify(data.options));
+		    	
+		    	$("#main_container").alpaca(data);
+	        }
+
+		    funcrion_render_alpaca(data);
+			 
+		});
 		</script>
